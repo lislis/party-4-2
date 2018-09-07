900a8e4708d6ef6b6cc3b06b82a2b2ef05d5dac7
lislis <mail@lislis.de>
2018-09-07 17:06:11 +0200

    gamepad

--- src/index.js
+++ src/index.js
@@ -1,82 +1,20 @@
 import JukeGen from 'jukegen';
-//import Gamepad from './gamepad';
-
+import Gamepad  from './lib/gamepad';
 
 //const jg = new JukeGen();
 //jg.start();
 
-//const pad = new Gamepad(window, navigator);
-let gp, raf;
-
-let pressed = [];
-var button = {
-  0: 'red',
-  1: 'yellow',
-  2: 'blue',
-  3: 'green',
-  4: 'l1',
-  5: 'r1',
-  6: 'select',
-  7: 'start'
-}
-
-function getDpad(pad) {
-  if (pad.axes[0] === 1) {
-    return 'right';
-  } else if (pad.axes[0] === -1) {
-    return 'left';
-  }
-  if (pad.axes[1] === 1) {
-    return 'down';
-  } else if (pad.axes[1] === -1) {
-    return 'up';
-  }
-  return false;
-}
-
-function btnPressed(label, pressed) {
-  return (pressed.indexOf(label) !== -1);
-}
-
-window.addEventListener("gamepadconnected", function(e) {
-  gp = navigator.getGamepads()[e.gamepad.index];
-  console.log("Gamepad connected at index " + gp.index + ": " + gp.id + ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.");
-  updateGamepad();
-});
-
-
-function updateGamepad() {
-  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
-  if (!gamepads)
-    return;
-  gp = gamepads[0];
-
-  for (var b = 0; b < gp.buttons.length; b++) {
-    if (gp.buttons[b].pressed) {
-      if (pressed.indexOf(button[b]) === -1) {
-        pressed.push(button[b]);
-        console.log('Pressed ', button[b], pressed);
-      }
-    } else {
-      if (pressed.indexOf(button[b]) !== -1) {
-        pressed.splice(pressed.indexOf(button[b], 1));
-      }
-    }
-  }
-
-  raf = requestAnimationFrame(updateGamepad);
-};
-
-
-
+const gamep = new Gamepad();
 
 function draw() {
   //console.log(jg.getFft);
 
+  if (gamep.getPad && gamep.getDpad() !== false) console.log(gamep.getDpad())
+  if (gamep.getPad && gamep.getBtnPressed('A')) console.log('A')
+  if (gamep.getPad && gamep.getBtnPressed('B')) console.log('B')
+  if (gamep.getPad && gamep.getBtnPressed('X')) console.log('X')
+  if (gamep.getPad && gamep.getBtnPressed('Y')) console.log('Y')
 
-  if (gp) console.log(getDpad(gp))
-
-  //console.log(pad.gamepad);
   requestAnimationFrame(draw);
 }
 
---
+++ src/lib/gamepad.js
@@ -0,0 +1,70 @@
+export default class Gamepad {
+  constructor() {
+    this.index = 0;
+    this.pressed = [];
+    this.pad = false;
+    this.buttonMap = [
+      'A',
+      'B',
+      'X',
+      'Y',
+      'l',
+      'r',
+      'select',
+      'start'
+    ];
+
+    let self = this;
+    window.addEventListener("gamepadconnected", function(e) {
+      self.index = e.gamepad.index;
+      self.pad = navigator.getGamepads()[self.index];
+      console.log("pad connected");
+      self.update();
+    });
+  }
+
+  get getPad() {
+    return this.pad;
+  }
+
+  update() {
+    const gamepads = navigator.getGamepads();
+    if (gamepads) {
+      this.pad = gamepads[0];
+      for (var b = 0; b < this.pad.buttons.length; b++) {
+        let indexInMap = this.pressed.indexOf(this.buttonMap[b]);
+        if (this.pad.buttons[b].pressed) {
+          if (indexInMap === -1) {
+            this.pressed.push(this.buttonMap[b]);
+            //console.log(this.pressed);
+          }
+        } else {
+          if (indexInMap !== -1) {
+            this.pressed.splice(indexInMap, 1);
+          }
+        }
+      }
+    } else {
+      console.log('no pads found');
+    }
+    requestAnimationFrame(this.update.bind(this));
+  }
+
+  getDpad() {
+    if (this.pad.axes[0] === 1) {
+      return 'right';
+    } else if (this.pad.axes[0] === -1) {
+      return 'left';
+    }
+    if (this.pad.axes[1] === 1) {
+      return 'down';
+    } else if (this.pad.axes[1] === -1) {
+      return 'up';
+    }
+    return false;
+  }
+
+  getBtnPressed(btnLabel) {
+    return this.pressed.indexOf(btnLabel) !== -1;
+  }
+}
